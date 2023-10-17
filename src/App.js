f886598498a48ece0ba579c4339f5ed0e4597c08
Saeid JaliliBashashi <saeidemoun@gmail.com>
2023-10-17 17:39:47 +0330

    react tutorial was created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Shio startup is my life</p>
         <a
           className="App-link"
           href="https://reactjs.org"
